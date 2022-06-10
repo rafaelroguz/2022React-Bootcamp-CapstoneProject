35c0e1b2fe8a69324fbfd475b2ce5523f0be2c81
Rafael Rodriguez <rafael.roguz@gmail.com>
2022-06-10 00:09:34 -0500

    Restored HOME as default route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { ROUTES } from "utils/routes";
 
 function App() {
   // TODO: using context api to handle location until Router is implemented
-  const [location, setLocation] = useState(ROUTES.PRODUCTS);
+  const [location, setLocation] = useState(ROUTES.HOME);
 
   return (
     <LocationContext.Provider value={{ location, setLocation }}>
